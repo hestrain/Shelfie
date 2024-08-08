70137915d927c59aa6ecbdbf29441d3f407f305d
AllredW <allrewil@gmail.com>
2024-08-08 14:06:01 -0400

    updated Book model with properties

---
+++ models/Comment.js
@@ -0,0 +1,39 @@
+// Third-party Modules
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+
+// Local Modules
+const sequelize = require("../config/connection");
+
+class Book extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8, Infinity],
+      },
+      // Reminder- add any new columns you'd like to the User model here
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = User;
