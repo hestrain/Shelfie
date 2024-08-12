24796b9802270be0a9be4d8e318115223439d316
Heather Strain <64221009+hestrain@users.noreply.github.com>
2024-08-12 14:02:18 -0400

    search is now a different page than search-results

--- controllers/api/searchRoutes.js
+++ controllers/api/searchRoutes.js
@@ -1,4 +1,3 @@
-
 const router = require("express").Router();
 
 // protects routes from unauthorized access
@@ -6,62 +5,51 @@ const { withGuard } = require("../../utils/authGuard");
 
 // things that deal with the search functions
 
-const {Book, SearchedBook} = require("../../models");
+const { Book, SearchedBook } = require("../../models");
 const { logger } = require("sequelize/lib/utils/logger");
 
 //get the search results and render them????? except i wrote that in bookAPI.js
-router.get("/searchResults", withGuard, async (req, res) => {
-    try {
-      getBooks(req.body),
-      res.render("searchResults", {
+router.get("/search-results", withGuard, async (req, res) => {
+  try {
+    getBooks(req.body),
+      res.render("search-results", {
         bookResults,
         loggedIn: req.session.logged_in,
       });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
+//to add the selected searched book ot the database
+router.post("/addBook", async (req, res) => {
+  try {
+    console.log("addBook running");
 
-  
-  //to add the selected searched book ot the database
-  router.post("/addBook", async (req, res) => { 
-    try {
-      console.log('addBook running');
-      
-      console.log(req.body);
-      
-      const newBookData = await Book.create(
-req.body
-      );
-      res.json(newBookData);
-    } catch (err) {
-      console.log(err);
-      
-      res.status(400).json(err);
-    }
-  });
+    console.log(req.body);
 
+    const newBookData = await Book.create(req.body);
+    res.json(newBookData);
+  } catch (err) {
+    console.log(err);
 
-    //posts the search results to a new table and then redners them MAYBE LOL
-    router.post("/searchResults", async (req, res) => { 
-      try {
-        const searchResults = req.body;
-        
-        await SearchedBook.truncate();
+    res.status(400).json(err);
+  }
+});
 
-        const results = await SearchedBook.bulkCreate(
-          searchResults
-                );
-        
-        // res.render('search', {
-        //   searchResults, 
-        //   logged_in: req.session.logged_in 
-        // })
-        res.json(searchResults);
-      } catch (err) {
-        res.status(500).json(err);
-      }
-    });
-  
-  module.exports = router;
+//posts the search results to a new table and then redners them MAYBE LOL
+router.post("/searchResults", async (req, res) => {
+  try {
+    const searchResults = req.body;
+
+    await SearchedBook.truncate();
+
+    const results = await SearchedBook.bulkCreate(searchResults);
+
+    res.json(searchResults);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
