5d6efb28bef82d5f3e36aba0cef0570b7b2ee9dc
AllredW <allrewil@gmail.com>
2024-08-13 13:41:15 -0400

    created some code to render user comments with books, being postponed for future development

--- controllers/bookPageRoutes.js
+++ controllers/bookPageRoutes.js
@@ -1,10 +1,15 @@
 const router = require('express').Router();
 const Book = require('../models/Book');
+const Comment = require('../models/Comment');
 
 // ? route to get one Book
 router.get('/:id', async (req, res) => {
     try {
-      const bookData = await Book.findByPk(req.params.id);
+      const bookData = await Book.findByPk(req.params.id, {
+        include: [{
+          model: Comment
+        }]
+      });
       if (!bookData) {
         res.status(404).json({ message: 'A book with this id was not found' });
         return;
